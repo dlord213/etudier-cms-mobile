7eb17ae9bdfd83eb45948f5da8d48f6fdcfb5d67
dlord213 <lollingworld41@gmail.com>
2025-04-21 22:46:39 +0800

    app: refactorings & changes

--- app/(student)/quiz/[quiz].tsx
+++ app/(student)/quiz/[quiz].tsx
@@ -31,12 +31,43 @@ export default function Page() {
     setAnswers(updated);
   };
 
+  const checkProgress = async () => {
+    try {
+      const userId = await instance!.authStore.record!.id;
+      const record = await instance!
+        .collection("users_quiz_submissions")
+        .getFirstListItem(`user_id = '${userId}' && quiz_id = '${data.id}'`);
+
+      let correct = 0;
+      data.quiz.forEach((q: any, i: number) => {
+        if (answers![i] === q.answer) correct++;
+      });
+
+      const percentScore = Math.round((correct / data.quiz.length) * 100);
+
+      await instance!.collection("users_quiz_submissions").update(record.id, {
+        quiz_id: data.id,
+        answers: answers,
+        user_id: userId,
+        score: correct,
+        percent: percentScore,
+      });
+
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+
   const handleSubmit = async () => {
     setSubmitting(true);
     setErrorMessage(null);
 
+    const hasProgress = await checkProgress();
+    if (hasProgress) return;
+
     try {
-      const userId = instance.authStore.record!.id;
+      const userId = instance!.authStore.record!.id;
 
       let correct = 0;
       data.quiz.forEach((q: any, i: number) => {
@@ -69,7 +100,7 @@ export default function Page() {
       try {
         const record = await instance!
           .collection("quiz")
-          .getOne(quiz.toString());
+          .getOne(quiz.toString(), { expand: "teacher_id" });
 
         setAnswers(Array(record.quiz.length).fill(-1));
 
@@ -85,156 +116,182 @@ export default function Page() {
 
   const pages = [
     <>
-      <MaterialIcons
-        size={24}
-        name="arrow-back"
-        color={"#242424"}
-        onPress={() => router.back()}
-      />
-      <View className="flex-1">
-        <Text
-          className="text-2xl"
-          style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
-        >
-          {data.title}
-        </Text>
-        <Text
-          className="text-gray-400"
-          style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
-        >
-          {data.description}
-        </Text>
-      </View>
-
-      <Pressable
-        className="flex-row gap-2 px-6 py-4 bg-green-500 self-end place-self-end rounded-3xl"
-        onPress={() => setIndex(1)}
-      >
-        <MaterialIcons name="start" size={16} color={"#fefefe"} />
-        <Text
-          className="text-sm text-[#fefefe]"
-          style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
-        >
-          Start
-        </Text>
-      </Pressable>
-    </>,
-    <>
-      <MaterialIcons
-        size={24}
-        name="arrow-back"
-        color={"#242424"}
-        onPress={() => router.back()}
-      />
-      <Text
-        className="text-4xl"
-        style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
-      >
-        Questions
-      </Text>
-      <ScrollView contentContainerClassName="gap-4">
-        {data.quiz.map((q: any, qIndex: number) => (
-          <View
-            key={qIndex}
-            className="mb-4 p-4 bg-gray-50 border border-gray-300 rounded-xl shadow"
-          >
+      {data && (
+        <>
+          <MaterialIcons
+            size={24}
+            name="arrow-back"
+            color={"#242424"}
+            onPress={() => router.back()}
+          />
+          <View className="flex-1">
+            <View className="flex-row gap-2 items-center mb-2">
+              <MaterialIcons
+                name="account-circle"
+                size={24}
+                color={"#afafaf"}
+              />
+              <Text
+                style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
+                className="text-[#afafaf]"
+              >
+                {data!.expand!.teacher_id.name}
+              </Text>
+            </View>
             <Text
-              className="font-bold mb-2"
+              className="text-2xl"
               style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
             >
-              Question {qIndex + 1}
+              {data.title}
             </Text>
             <Text
-              className="mb-2"
+              className="text-gray-400"
               style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
             >
-              {q.question}
+              {data.description}
             </Text>
-
-            <View className="flex flex-col gap-2">
-              {Array.from({ length: Math.ceil(q.options.length / 2) }).map(
-                (_, rowIndex) => {
-                  const start = rowIndex * 2;
-                  const rowOptions = q.options.slice(start, start + 2);
-
-                  return (
-                    <View key={rowIndex} className="flex flex-row gap-2">
-                      {rowOptions.map((opt: string, oIndex: number) => {
-                        const optionIndex = start + oIndex;
-                        return (
-                          <Pressable
-                            key={optionIndex}
-                            onPress={() => handleSelect(qIndex, optionIndex)}
-                            disabled={submitted}
-                            className={`flex-1 px-4 py-2 rounded-3xl ${
-                              answers[qIndex] === optionIndex
-                                ? "bg-blue-500"
-                                : "bg-gray-100 border border-gray-300"
-                            }`}
-                          >
-                            <Text
-                              className={`${
-                                answers[qIndex] === optionIndex
-                                  ? "text-white"
-                                  : "text-gray-800"
-                              }`}
-                              style={{
-                                fontFamily: WorkSansFonts.WorkSans_400Regular,
-                              }}
-                            >
-                              {opt}
-                            </Text>
-                          </Pressable>
-                        );
-                      })}
-                      {/* Fill empty space if only one item in row */}
-                      {rowOptions.length === 1 && <View className="flex-1" />}
-                    </View>
-                  );
-                }
-              )}
-            </View>
           </View>
-        ))}
-      </ScrollView>
-      {!submitted ? (
-        <Pressable
-          onPress={handleSubmit}
-          disabled={submitting}
-          className="bg-green-500 p-4 rounded-2xl items-center"
-        >
-          {submitting ? (
-            <ActivityIndicator color="#fff" />
-          ) : (
+
+          <Pressable
+            className="flex-row gap-2 px-6 py-4 bg-green-500 self-end place-self-end rounded-3xl"
+            onPress={() => setIndex(1)}
+          >
+            <MaterialIcons name="start" size={16} color={"#fefefe"} />
             <Text
-              className="text-white font-semibold"
-              style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
+              className="text-sm text-[#fefefe]"
+              style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
             >
-              Submit Answers
+              Start
             </Text>
-          )}
-        </Pressable>
-      ) : (
-        <View className="mt-6 space-y-2">
+          </Pressable>
+        </>
+      )}
+    </>,
+    <>
+      {data && (
+        <>
+          <MaterialIcons
+            size={24}
+            name="arrow-back"
+            color={"#242424"}
+            onPress={() => router.back()}
+          />
           <Text
-            className="text-green-600 font-bold"
+            className="text-4xl"
             style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
           >
-            Quiz submitted successfully!
+            Questions
           </Text>
-          <Text
-            className="text-lg"
-            style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
-          >
-            Score: {score} / {data.quiz.length}
-          </Text>
-          <Text
-            className="text-lg"
-            style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
-          >
-            Percentage: {percent}%
-          </Text>
-        </View>
+          <ScrollView contentContainerClassName="gap-4">
+            {data.quiz.map((q: any, qIndex: number) => (
+              <View
+                key={qIndex}
+                className="mb-4 p-4 bg-gray-50 border border-gray-300 rounded-xl shadow"
+              >
+                <Text
+                  className="font-bold mb-2"
+                  style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
+                >
+                  Question {qIndex + 1}
+                </Text>
+                <Text
+                  className="mb-2"
+                  style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
+                >
+                  {q.question}
+                </Text>
+
+                <View className="flex flex-col gap-2">
+                  {Array.from({ length: Math.ceil(q.options.length / 2) }).map(
+                    (_, rowIndex) => {
+                      const start = rowIndex * 2;
+                      const rowOptions = q.options.slice(start, start + 2);
+
+                      return (
+                        <View key={rowIndex} className="flex flex-row gap-2">
+                          {rowOptions.map((opt: string, oIndex: number) => {
+                            const optionIndex = start + oIndex;
+                            return (
+                              <Pressable
+                                key={optionIndex}
+                                onPress={() =>
+                                  handleSelect(qIndex, optionIndex)
+                                }
+                                disabled={submitted}
+                                className={`flex-1 px-4 py-2 rounded-3xl ${
+                                  answers[qIndex] === optionIndex
+                                    ? "bg-blue-500"
+                                    : "bg-gray-100 border border-gray-300"
+                                }`}
+                              >
+                                <Text
+                                  className={`${
+                                    answers[qIndex] === optionIndex
+                                      ? "text-white"
+                                      : "text-gray-800"
+                                  }`}
+                                  style={{
+                                    fontFamily:
+                                      WorkSansFonts.WorkSans_400Regular,
+                                  }}
+                                >
+                                  {opt}
+                                </Text>
+                              </Pressable>
+                            );
+                          })}
+                          {/* Fill empty space if only one item in row */}
+                          {rowOptions.length === 1 && (
+                            <View className="flex-1" />
+                          )}
+                        </View>
+                      );
+                    }
+                  )}
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+          {!submitted ? (
+            <Pressable
+              onPress={handleSubmit}
+              disabled={submitting}
+              className="bg-green-500 p-4 rounded-2xl items-center"
+            >
+              {submitting ? (
+                <ActivityIndicator color="#fff" />
+              ) : (
+                <Text
+                  className="text-white font-semibold"
+                  style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
+                >
+                  Submit Answers
+                </Text>
+              )}
+            </Pressable>
+          ) : (
+            <View className="mt-6 space-y-2">
+              <Text
+                className="text-green-600 font-bold"
+                style={{ fontFamily: WorkSansFonts.WorkSans_700Bold }}
+              >
+                Quiz submitted successfully!
+              </Text>
+              <Text
+                className="text-lg"
+                style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
+              >
+                Score: {score} / {data.quiz.length}
+              </Text>
+              <Text
+                className="text-lg"
+                style={{ fontFamily: WorkSansFonts.WorkSans_400Regular }}
+              >
+                Percentage: {percent}%
+              </Text>
+            </View>
+          )}
+        </>
       )}
     </>,
   ];
